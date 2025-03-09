e277b6947a7ec80e322cb618b71e180d6c1d9599
Yuji-Guilherme <guilherme.nagasaki@outlook.com>
2025-03-09 12:56:05 -0300

    feat: add functionality

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const friends = [];
+
+const getName = () => document.getElementById('amigo').value.trim();
+
+const getList = () => document.getElementById('listaAmigos');
+
+const cleanInput = () => document.getElementById('amigo').value = '';
+
+const cleanHtmlList = () => {
+    const list = getList();
+    list.innerHTML = '';
+}
+
+const addFriend = () => {
+    const name = getName();
+
+    if (name == '') {
+        alert('Por favor, insira um nome.');
+        cleanInput();
+        return;
+    }
+
+    attFriendsList(name);
+
+    cleanInput();
+};
+
+const attFriendsList = (name) => {
+    friends.push(name);
+
+    cleanHtmlList();
+
+    const list = getList();
+
+    for (let i = 0; i < friends.length; i++) {
+        const li = document.createElement('li'); 
+        li.textContent = friends[i]; 
+        list.appendChild(li);
+    }
+}
+
+const drawFriend = () => {
+    if (friends.length == 0) return;
+
+    const result = Math.floor(Math.random() * friends.length);
+    const randomName = friends[result];
+    
+    cleanHtmlList();
+
+    const htmlResult = document.getElementById('resultado');
+    htmlResult.innerHTML = `O(A) amigo(a) sorteado(a) é: ${randomName}`;
+}
